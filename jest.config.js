b7a1a3cb044e1706ffbb3e2284d763d3aae8e6ed
sameera <www.sameera@gmail.com>
2021-11-17 17:35:38 +0530

    chore:rename project

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    displayName: "contrib-views",
+    displayName: "contrib-routing",
     preset: "../../../jest.preset.js",
     setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
     globals: {
@@ -8,7 +8,7 @@ module.exports = {
             stringifyContentPathRegex: "\\.(html|svg)$",
         },
     },
-    coverageDirectory: "../../../coverage/libs/contrib/views",
+    coverageDirectory: "../../../coverage/libs/contrib/routing",
     transform: {
         "^.+\\.(ts|js|html)$": "jest-preset-angular",
     },
