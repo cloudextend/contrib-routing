772922fc389dba1f4cebadb3a8a11a238817a7e2
sameera <www.sameera@gmail.com>
2022-03-19 06:41:30 +0530

    fix: minor linting fix

--- src/lib/navigations/routing.module.ts
+++ src/lib/navigations/routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
+
 import { NavigationEffects } from "./navigation.effects";
 
 @NgModule({
