4018d01cf96d1b1e57430fc9d26ab05278e1fbfd
sameera <www.sameera@gmail.com>
2022-03-21 07:52:33 +0530

    chore: minor linting fix

--- src/lib/router-state/router-state.module.ts
+++ src/lib/router-state/router-state.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { routerReducer, StoreRouterConnectingModule } from "@ngrx/router-store";
 import { StoreModule } from "@ngrx/store";
+
 import { CustomSerializer } from "./custom-route-serializer";
 import { STATE_KEY } from "./location-state";
 
